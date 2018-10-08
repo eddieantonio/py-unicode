987c05d099f8af4b1fc8a26a94e17fcc86a75028
Eddie Antonio Santos <easantos@ualberta.ca>
2018-10-08 14:57:26 -0600

    Get some work started.

---
+++ curie.py
@@ -0,0 +1,20 @@
+
+with open('letter.txt', 'w') as letter:
+    letter.write("""
+        Chère Madame,
+
+        Et maintenant  le voyage a la supermarche!
+        Pamplemousse
+        Ananas
+        Jus d'orange
+        Boeuf
+        Soup du jour
+        Camembert
+        Jacques Cousteau
+        Baguette
+
+        Salutations distinguées,
+        Marie Skłodowska Curie
+    """)
+
+# Lyrics by McKenzie, Bret Peter T, and Clement, Jemaine Atea Mahana
